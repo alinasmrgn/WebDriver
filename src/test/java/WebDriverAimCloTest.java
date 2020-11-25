4c05ad192646b4039840594e6c3e04b586abc80c
Alina Khokhryakova <a.smorgyn@mail.ru>
2020-11-25 20:31:05 +0300

    WebDriver

---
+++ src/test/java/WebDriverAimCloTest.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class WebDriverAimCloTest {
+    public static String expectedResult ="Распечатать" ;
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void driverSetup(){ driver = new ChromeDriver(); }
+
+    @Test
+    public void addToBagTest() {
+        driver.get("https://aimclo.ru/shop/kardigany/kardigan-udlinennyy-seryy/");
+        WebElement addToBagButton = waitForElementLocatedBy(driver,By.xpath("//*[@value='ДОБАВИТЬ В КОРЗИНУ']"));
+        addToBagButton.click();
+        WebElement goToBagButton = driver.findElement(By.xpath("//a[@href='/basket/']"));
+        goToBagButton.click();
+        WebElement goToLookBagButton = driver.findElement(By.xpath("//div[@class='add-basket__footer']"));
+        goToLookBagButton.click();
+        Assert.assertEquals(waitForElementLocatedBy(driver,By.xpath("//div[@class='cart-buttons__buttons-wrap']")).getText(), expectedResult);
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void driverShutDown(){
+        driver.quit();
+        driver=null;
+    }
+    private static WebElement waitForElementLocatedBy(WebDriver driver, By by){
+        return new WebDriverWait(driver,4)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+
+}
+
